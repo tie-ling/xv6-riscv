958fc0b3d5beed7fe13abb55c55cdb2359115cb8
Yan Cao <yc@example.com>
2024-11-29 12:51:35 +0100

    flake
    Signed-off-by: Yan Cao <yc@example.com>

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/f45667df53b4a4bb7b0bc0fa4fb83e8c0c51add5";
+  # https://pdos.csail.mit.edu/6.1810/
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      crossPkgs = (
+        pkgs {
+          # uses GCC and newlib
+          crossSystem = {
+            system = "riscv64-none-elf";
+          };
+        }
+      );
+    in
+    {
+      formatter.x86_64-linux = pkgs.nixfmt-rfc-style;
+      devShell.x86_64-linux = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          qemu_full
+          gnumake
+          crossPkgs.gcc
+        ];
+      };
+    };
+}
